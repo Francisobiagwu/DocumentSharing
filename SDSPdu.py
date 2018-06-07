1bc60989a71d02f7d7acd3b074aa332db22faac5
francisobiagwu <francisobiagwu@gmail.com>
2018-06-06 21:20:16 -0400

    test

--- SDSPdu.py
+++ SDSPdu.py
@@ -12,14 +12,16 @@ class SDSPdu:
     message_type_len = '12s'
     checksum_len = 'q'
     error_code = 'i'
+    data = '100s'
     more_data = '?'
     space = ' '
 
-    format = message_type_len + space + checksum_len + space + error_code + space + more_data
+    format = message_type_len + space + checksum_len + space + error_code + space + data + space + more_data
 
     s = struct.Struct(format)
     print(format)
-    array = (b'test', 888, 89, True)
+    array = (b'test', 888, 89, b'data is the man', True)
+    print(s.size)
     pdu_packed = s.pack(*array)
     print(pdu_packed)
 
