30dd548f3b09436351da129c5e425a9a3ccd3c3d
francisobiagwu <francisobiagwu@gmail.com>
2018-06-08 07:34:36 -0400

    update

--- Server/SDSServer.py
+++ Server/SDSServer.py
@@ -8,10 +8,6 @@
 
 # !/usr/bin/env python
 
-__author__ = "Francis Obiagwu"
-__copyright__ = "Copyright 2017, GSCE IDS Project"
-__version__ = "1.0"
-__email__ = "francis.c.obiagwu.civ@mail.mil"
 
 import socket
 import threading
