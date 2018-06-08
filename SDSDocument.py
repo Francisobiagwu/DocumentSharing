f37d18c06f85ab10c7c23214c36ef10ea664b1c9
francisobiagwu <francisobiagwu@gmail.com>
2018-06-07 23:12:10 -0400

    update

--- SDSDocument.py
+++ SDSDocument.py
@@ -18,9 +18,13 @@ class SDSDocument:
     def __init__(self, path=__default_path):
         if path is None:  # is during the initialization there no path is specified, then use the default path
             self.__path = self.__default_path
+            print(self.__path)
 
         else:  # if the during the initialization, the user specified a path, then use the specified path
             self.__path = path
+            print('in else')
+            print(self.__path)
+
 
     def get_document(self):
         """
@@ -28,6 +32,9 @@ class SDSDocument:
         :return:
         """
         try:
+            print(self.__path)
+            '/Users/francisobiagwu/PycharmProjects/SecureDocumentSharing/document.txt'
+
             with open(self.__path, 'r', encoding='utf-8') as file:
                 print('about getting document.txt')
                 document = file.read()
