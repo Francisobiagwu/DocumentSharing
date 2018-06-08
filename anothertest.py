1ea384e033c647566e57717b3a24bec13d381d14
francisobiagwu <francisobiagwu@gmail.com>
2018-06-07 22:01:27 -0400

    update

--- anothertest.py
+++ anothertest.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-__author__ = "Francis Obiagwu"
-__copyright__ = "Copyright 2017, GSCE IDS Project"
-__version__ = "1.0"
-__email__ = "francis.c.obiagwu.civ@mail.mil"
 
 
 class Header:
