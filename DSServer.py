7f6a9b74fcfc950b0046c50df9300fd1c7be185a
Francis Obiagwu <francisobiagwu@gmail.com>
2018-06-21 14:18:39 -0400

    Update DSServer.py

--- DSServer.py
+++ DSServer.py
@@ -4,18 +4,18 @@ __author__ = "Francis Obiagwu"
 __copyright__ = "Copyright 2018"
 __version__ = "1.0"
 
-import binascii
 import socket
 import threading
+from DSPdu import DSPdu
+from DSState import DSState
 from datetime import datetime
-
-from DSCodes import DSCode
+import binascii
 from DSDocument import DSDocument
+from DSCodes import DSCode
 from DSFlags import DSFlags
 from DSMessageType import DSMessageType
-from DSPdu import DSPdu
+
 from DSServerLogManagement import DSServerLogManagement
-from DSState import DSState
 
 
 class DSServer():
@@ -99,7 +99,7 @@ class DSServer():
             message_type = unpacked_pdu_no_pad[self.message_type_index].encode()
             data = unpacked_pdu_no_pad[self.data_index]
             section_id = unpacked_pdu_no_pad[self.section_id_index]
-            flag = unpacked_pdu_no_pad[self.flag_index].encode()
+            flag = unpacked_pdu_no_pad[self.flag_index]
             print(current_state, message_type)
             if message_type == DSMessageType.CONNECT:
                 pass
@@ -110,27 +110,16 @@ class DSServer():
                 self.s_edit(section_id, client_pdu, client_state, client_socket, client_address)
 
             elif message_type == DSMessageType.S_COMMIT:
-                print(data, flag)
-                print('data before the concatonation: {}'.format(data))
-                print(client_pdu, unpacked_pdu, unpacked_pdu_no_pad)
                 if flag == DSFlags.more:
+
                     self.string_builder +=data
                     print('string '.format(self.string_builder))
 
-                else:  # flag == DSFlags.finish:
-                    if self.string_builder != '':  # if string builder contains something
-                        data = self.string_builder
-                        print('data: {}'.format(data))
-                        self.string_builder = ''  # reset string builder
+                else:
+                    data = self.string_builder
+                    print(data)
 
-                    else:
-                        # if string builder is None, then data that was sent for commit is equal to the size of struct
-                        pass
-
-                self.s_commit(section_id, data, client_pdu, client_state, client_socket, client_address)
-
-
-
+                    self.s_commit(section_id, data, client_pdu, client_state, client_socket, client_address)
 
             elif message_type == DSMessageType.S_RELEASE:
                 self.release(section_id, client_pdu, client_state, client_socket, client_address)
@@ -177,14 +166,14 @@ class DSServer():
         client_socket.send(pdu)
         self.server_log_manager.add_authenticated_client_connection(client_socket, client_address)
 
-        # Now send the document.txt to the client
-        data_string = self.ds_document.get_document_as_string()  # get the entire document.txt as string
+        # Now send the document to the client
+        data_string = self.ds_document.get_document_as_string()  # get the entire document as string
         data_break_down = self.ds_document.break_data(data_string)
 
         freq_to_send = len(data_break_down)
         count = 0
 
-        for item in data_break_down:  # we don't care about the document.txt flags about sections taken/free
+        for item in data_break_down:  # we don't care about the document flags about sections taken/free
             count += 1
             timestamp = client_pdu.get_time()  # get timestamp
             error_code = DSCode.LOGIN_SUCCESS  # assign error code
@@ -201,6 +190,10 @@ class DSServer():
             pdu_array = [DSMessageType.CAUTH, timestamp, error_code, flag, reserved_1, reserved_2, section_id, data,
                          checksum]
             pdu = client_pdu.pack(pdu_array)
+            ##################################
+            # set state#
+            ##################################
+            client_state.set_state(DSMessageType.CAUTH)
             #############################################
             # SEND THE DOCUMENT
             #############################################
@@ -210,6 +203,11 @@ class DSServer():
 
     def s_edit( self, section_id, client_pdu, client_state, client_socket, client_address ):
         section_id = int(section_id)
+        print('ds_document')
+        print('-------------------')
+        print(self.ds_document)
+        print('--------------------')
+        print(section_id)
         print(self.ds_document.get_document_sections())
         if section_id in self.ds_document.get_document_sections():
             print('Before')
@@ -258,7 +256,7 @@ class DSServer():
 
 
             else:  # data is not free
-                # find out who is the current owner of the document.txt
+                # find out who is the current owner of the document
                 #
                 current_section_owners = self.server_log_manager.get_section_owners()
                 data = b''
@@ -323,14 +321,14 @@ class DSServer():
             client_socket.send(pdu)
             self.server_log_manager.add_authenticated_client_connection(client_socket, client_address)
 
-            # Now send the document.txt to the client
-            data_string = self.ds_document.get_document_as_string()  # get the entire document.txt as string
+            # Now send the document to the client
+            data_string = self.ds_document.get_document_as_string()  # get the entire document as string
             data_break_down = self.ds_document.break_data(data_string)
 
             freq_to_send = len(data_break_down)
             count = 0
 
-            for item in data_break_down:  # we don't care about the document.txt flags about sections taken/free
+            for item in data_break_down:  # we don't care about the document flags about sections taken/free
                 count += 1
                 timestamp = client_pdu.get_time()  # get timestamp
                 error_code = DSCode.COMMIT_UPDATE  # assign error code
@@ -431,11 +429,5 @@ class DSServer():
 
 
 if __name__ == '__main__':
-    try:
-        server = DSServer()
-        server.start()
-
-    except (KeyboardInterrupt, OSError) as err:
-        import sys
-
-        sys.exit(1)
+    server = DSServer()
+    server.start()
