8792a99d6a05ceb4cfb5b911ef08945f58ac19e2
Francis Obiagwu <francisobiagwu@gmail.com>
2018-06-21 14:26:54 -0400

    Update SDSTimer.py

--- SDSTimer.py
+++ SDSTimer.py
@@ -8,23 +8,33 @@
 import time
 
 
-class SDSTimer:
+class DSTimer:
     count_down = 5  # seconds
-    inactivity = 30  # seconds
-    timer_finished = False
 
-    def __int__(self, caller_countdown=count_down):  # use default if the caller didn't specify
+    def __init__( self, caller_countdown=count_down ):  # use default if the caller didn't specify
         self.count_down = caller_countdown
+        self.inactivity = 30  # seconds
+        self.timer_finished = False
+        self.is_ACK_received = False
 
-    def start_timer(self, delay=count_down):
+    def start_timer( self, delay=count_down ):
         self.count_down = delay
-        while self.count_down:  # will stop once the count_down reaches 0
+        # will stop once the count_down reaches 0 or when an ACK is received
+        print('in start timer')
+        # print('countdown {} is_ack_received: {}'.format(self.count_down, self.is_ACK_received))
+        # print(self.count_down, self.is_ACK_received)
+        while self.count_down and not self.is_ACK_received:
+            print(self.is_ACK_received)
             print('Timer: {}'.format(self.count_down))
             time.sleep(1)
             self.count_down -= 1
 
         self.timer_finished = True
 
-    def check_inactivity(self):
+    def check_inactivity( self ):
         while self.inactivity:
             print('Inactivity: {}'.format(self.inactivity))
+
+
+
+
