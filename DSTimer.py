1e829a5f72aebeec50aa7be6f1e70de4ce4d0bf9
Francis <fco25@drexel.edu>
2018-07-02 18:35:16 -0400

    update

--- DSTimer.py
+++ DSTimer.py
@@ -30,36 +30,31 @@ class DSTimer:
         # print('countdown {} is_ack_received: {}'.format(self.count_down, self.is_ACK_received))
         # print(self.count_down, self.is_ACK_received)
         while self.count_down and not self.is_ACK_received:
-            if self.count_down == 0:
-                recently_sent_data = self.error_correction_obj.sent_data
-                # resend the data to the client
-                # so I need client_socket, and I will need to call the pdu class in order to do this
-                timestamp = self.client_pdu_obj.get_time()  # get timestamp
-                reserved_1 = self.null_byte
-                reserved_2 = self.null_byte
-                ACK_data = self.null_byte
-                checksum = self.client_pdu_obj.get_checksum(timestamp, ACK_data)
-                pdu_array = [DSMessageType.CAUTH, timestamp, DSCode.LOGIN_SUCCESS, DSFlags.finish, reserved_1,
-                             reserved_2,
-                             self.null_byte, ACK_data, checksum]
+            print(self.is_ACK_received)
+            print('Timer: {}'.format(self.count_down))
+            time.sleep(1)
+            self.count_down -= 1
 
-                pdu = self.client_pdu.pack(pdu_array)
-                self.client_socket.send(pdu)
-                self.server_log_manager.add_authenticated_client_connection(self.client_socket, self.client_address)
+        if self.count_down == 0 and self.is_ACK_received:  # if the timer stops and the ACK is not received
+            recently_sent_data = self.error_correction_obj.sent_data
+            # resend the data to the client
+            # so I need client_socket, and I will need to call the pdu class in order to do this
+            timestamp = self.client_pdu_obj.get_time()  # get timestamp
+            reserved_1 = self.null_byte
+            reserved_2 = self.null_byte
+            ACK_data = self.null_byte
+            checksum = self.client_pdu_obj.get_checksum(timestamp, ACK_data)
+            pdu_array = [DSMessageType.CAUTH, timestamp, DSCode.LOGIN_SUCCESS, DSFlags.finish, reserved_1,
+                         reserved_2,
+                         self.null_byte, ACK_data, checksum]
 
-                # Now send the document.txt to the client
-                data_string = self.ds_document.get_document_as_string()  # get the entire document.txt as string
-                data_break_down = self.ds_document.break_data(data_string)
+            pdu = self.client_pdu.pack(pdu_array)
+            self.client_socket.send(pdu)
+            self.server_log_manager.add_authenticated_client_connection(self.client_socket, self.client_address)
 
-                freq_to_send = len(data_break_down)
-
-                pass
-
-            else:
-                print(self.is_ACK_received)
-                print('Timer: {}'.format(self.count_down))
-                time.sleep(1)
-                self.count_down -= 1
+            # Now send the document.txt to the client
+            self.client_socket.send(recently_sent_data)
+            pass
 
         self.timer_finished = True
 
