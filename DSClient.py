ef84be3bc0e70f29af0ea741242123e7657a94f6
Francis Obiagwu <francisobiagwu@gmail.com>
2018-08-02 15:38:30 -0400

    Update DSClient.py

--- DSClient.py
+++ DSClient.py
@@ -9,6 +9,7 @@
 import socket
 import sys
 import threading
+import os
 
 from DSCodes import DSCode
 from DSInput import DSInput
@@ -27,22 +28,21 @@ class DSClient:
     __CLIENT__SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     placement = 0
 
-    def __init__( self, buffer_size=__BUFFER_SIZE ):
-        self.__BUFFER_SIZE = buffer_size
+    def __init__(self):
         self.client_pdu = DSPdu()  # create new pdu object
         self.allowed_data_size = self.client_pdu.get_data_size()
-        self.__BUFFER_SIZE = self.client_pdu.get_size()  # set the size of the pdu
+        self.__BUFFER_SIZE = self.client_pdu.get_buffer_size()  # set the size of the pdu
         self.done_processing = False
         self.client_ds_code = DSCode()
-        self.request_index, self.timestamp_index, self.error_code_index, self.flag_index, self.reserved_1_index, self.reserved_2_index, self.reserved_3_index, self.data_index, self.checksum_index = DSPdu().get_pdu_parts_index()
+        self.message_type_index, self.timestamp_index, self.error_code_index, self.flag_index,  self.changed_section_index, self.section_id_index, self.reserved_1_index, self.reserved_2_index, self.reserved_3_index, self.data,self.data_size_index, self.checksum_index = DSPdu().get_pdu_parts_index()
         self.null_byte = b'\x00'  # used in place of data when is not need to send data during S_DENIED, CAUTH etc
         self.input_processor = DSInput()
         self.server_response_processor = DSClientServerResponseProcessor()
-        has_token = False  # used to track if the client has been issued a token
 
         self.first_message_recvd = False  # used to track if the client have received the first 'CONNECT' message
         self.placement = 0
         self.is_authenticated = False
+        self.server_alive = False
 
     def start( self ):
         """
@@ -51,10 +51,11 @@ class DSClient:
         """
         try:
             self.__CLIENT__SOCKET.connect((self.__TCP_IP, self.__PORT))
-            print('client successfully connected')
+            self.server_alive = True
+            # print('client successfully connected')
         except ConnectionRefusedError as err:
             print(err.args)
-            sys.exit(1)
+            os._exit(1)
 
         #################################################
         # receive and send the first message
@@ -69,34 +70,27 @@ class DSClient:
                 break
             else:
                 print('The server doesn\'t want to talk to you')
-                sys.exit(1)
+                os._exit(1)
 
         # run the thread to receive pdu from the server, this thread runs forever
         recv_thread = threading.Thread(target=self.receiving_thread).start()
 
-        while True:
-
+        while self.server_alive:
             array, string_array = self.input_processor.get_user_input()  # return the user input as array and as string
-            #print(array)
             if array[0] == 'COMMIT':
                 # process differently
                 # print(array, string_array)
                 commit_pdu_array = self.input_processor.process_user_input(array, string_array)
-                #print(commit_pdu_array)
-                # print('---------------------------')
-                # print(commit_pdu_array)
-                # print('---------------------------')
                 for item in commit_pdu_array:
-                    #print(item)
+                    print(item)
                     pdu = self.client_pdu.pack(item)
                     self.__CLIENT__SOCKET.send(pdu)
-                pass
-
+                    print('packed pdu sent: {}'.format(pdu))
 
             else:
                 pdu_array = self.input_processor.process_user_input(array, string_array)  # obtain the pdu as byte
+                print(pdu_array)
                 pdu = self.client_pdu.pack(pdu_array)
-
                 self.__CLIENT__SOCKET.send(pdu)  # send
 
     def receiving_thread( self ):
@@ -104,7 +98,7 @@ class DSClient:
         Used to run the receiving thread forever
         :return: None
         """
-        while True:
+        while self.server_alive: # if the server is still listening for this client
             try:
                 pdu = self.__CLIENT__SOCKET.recv(self.__BUFFER_SIZE)
                 # print(pdu)
@@ -114,7 +108,10 @@ class DSClient:
 
             except ConnectionResetError as err:
                 print(err.args)
-                break
+                self.server_alive = False
+
+                os._exit(1)
+
 
 
 if __name__ == '__main__':
@@ -123,4 +120,4 @@ if __name__ == '__main__':
         a.start()
     except (KeyboardInterrupt, EOFError, OSError, IOError) as err:
         print(err.args)
-        sys.exit(1)
+        os._exit(1)
